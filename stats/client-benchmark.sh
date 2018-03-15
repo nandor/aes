3eb778a3ff32dd098fe7ddd25c9ae9a2eb5b105c
root <david.greaves@cl.cam.ac.uk>
2018-03-15 18:42:49 +0000

    minor

---
+++ stats/client-benchmark.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Runs benchmark on the Parallela card. Output should be piped into a file.
+
+KEY=00112233445566778899aabbccddeeff
+FILE=input16.txt
+ITERS=5
+
+
+for ((s = 4; s <= 12; ++s)) do
+  devmem2 0xF8000170 w 0x100$(printf %x $s)00 > /dev/null
+  cat topfpga.bit > /dev/xdevcfg
+
+  for ((i = 0; i < $ITERS; ++i)) do
+    ./aes/client/client $KEY $KEY $FILE
+  done
+done
+
+for ((i = 0; i < $ITERS; ++i)) do
+  ./aes/unix/aes $KEY $KEY $FILE
+done
